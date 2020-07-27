243bddd2f10c502a1b9c171d440c260645c89c36
sayali1098 <sauamritkar@gmail.com>
2020-07-27 17:22:28 +0530

    FSD0620B102

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BioComponent } from './bio/bio.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BioComponent,
+    PortfolioComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
